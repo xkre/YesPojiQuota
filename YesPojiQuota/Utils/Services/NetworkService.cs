539c068915534343f9f8a31e810d741255b9b466
xkre <xkre@live.com>
2017-04-13 16:10:11 +0800

    Added INetworkService and NetworkService classes

---
+++ YesPojiQuota/Utils/Services/NetworkService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YesPojiQuota.Utils.Interfaces;
+
+namespace YesPojiQuota.Utils.Services
+{
+    public class NetworkService : INetworkService
+    {
+    }
+}
