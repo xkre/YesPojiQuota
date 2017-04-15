ee2945bc4fc6f13f3f3b63d2f1e63b8d336a0577
xkre <xkre@live.com>
2017-04-15 13:21:23 +0800

    Added NetworkService

---
+++ YesPojiQuota/Utils/Enums/NetworkCondition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YesPojiQuota.Utils.Enums
+{
+    public enum NetworkCondition
+    {
+        Online,
+        YesWifiConnected,
+        NotConnected
+    }
+}
