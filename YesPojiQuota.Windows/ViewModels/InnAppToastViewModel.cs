0d77f60b98c66dafd0ae53a2adfe9574a2460f7e
xkre <arekram@live.com>
2017-04-21 16:14:48 +0800

    Implemented Logout, network monitor

--- YesPojiQuota.Windows/ViewModels/InnAppToastViewModel.cs
+++ YesPojiQuota.Windows/ViewModels/InnAppToastViewModel.cs
@@ -1,4 +1,6 @@
-﻿using GalaSoft.MvvmLight.Messaging;
+﻿using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Threading;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +15,12 @@ namespace YesPojiQuota.ViewModels
     public class InnAppToastViewModel : MainViewModel
     {
         private INetworkService _ns;
+        private ILoginService _ls;
 
-        public InnAppToastViewModel(INetworkService ns)
+        public InnAppToastViewModel(INetworkService ns, ILoginService ls)
         {
             _ns = ns;
+            _ls = ls;
         }
 
         #region Properties
@@ -33,46 +37,92 @@ namespace YesPojiQuota.ViewModels
             get { return _message; }
             set { Set("Message", ref _message, value); }
         }
+
+        private bool _isConnected;
+        public bool IsConnected
+        {
+            get { return _isConnected; }
+            set { Set("IsConnected", ref _isConnected, value); }
+        }
+
         #endregion Properties
-
-
 
         public override async Task InitAsync()
         {
             await base.InitAsync();
 
-            _ns.NetworkChanged += ProcessNotification;
-            await _ns.CheckConnectionAsync();
+            _ns.NetworkChanged += ProcessNetworkNotification;
+            await Task.Run(() =>
+            {
+                _ns.CheckConnectionAsync();
+                _ns.StartMonitor();
+            });
 
             Messenger.Default.Register<string>(this, HandleNotification);
-
+            //TODO: Temporary   -- Seriously -------------------------------------
+            Messenger.Default.Register<bool>(this, HandleNotification);
+            //Temporary   -- Seriously -------------------------------------
         }
 
-        private async void HandleNotification(string a)
+        private void HandleNotification(string a)
         {
             Message = a;
         }
 
-
-        public void ProcessNotification(NetworkCondition condition)
+        private void HandleNotification(bool a)
         {
-            switch (condition)
+            IsConnected = a;
+        }
+
+        public async void Logout()
+        {
+            var success = await _ls.LogoutAsync();
+
+            if (success)
             {
-                case NetworkCondition.NotConnected:
-                    Message = "Not online";
-                    break;
-                case NetworkCondition.Online:
-                    Message = "Connected";
-                    break;
-                case NetworkCondition.YesWifiConnected:
-                    Message = "Login Required";
-                    break;
-                case NetworkCondition.OnlineNotYes:
-                    Message = "Not on Yes Network";
-                    break;
+                Message = "Logout Successfull";
             }
 
-            Visibility = true;
+            IsConnected = false;
+        }
+
+        //private RelayCommand _logoutCommand;
+        //public RelayCommand LogoutCommand
+        //{
+        //    get
+        //    {
+        //        return _logoutCommand ?? (_logoutCommand = new RelayCommand(
+        //                () => Logout(), 
+        //                () => IsConnected)
+        //            );
+        //    }
+        //}
+
+
+        public async void ProcessNetworkNotification(NetworkCondition condition)
+        {
+            await DispatcherHelper.RunAsync(()=>
+            {
+                IsConnected = false;
+                switch (condition)
+                {
+                    case NetworkCondition.NotConnected:
+                        Message = "Not online";
+                        break;
+                    case NetworkCondition.Online:
+                        Message = "Connected";
+                        IsConnected = true;
+                        break;
+                    case NetworkCondition.YesWifiConnected:
+                        Message = "Login Required";
+                        break;
+                    case NetworkCondition.OnlineNotYes:
+                        Message = "Online, not on Yes Network";
+                        break;
+                }
+
+                Visibility = true;
+            });
         }
     }
 }
