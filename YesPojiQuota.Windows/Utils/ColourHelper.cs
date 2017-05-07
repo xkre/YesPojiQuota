3c3f7b6e70b5081007764503af2b721d0613b482
xkre <xkre@live.com>
2017-05-07 15:58:41 +0800

    Added ColourHelper

---
+++ YesPojiQuota.Windows/Utils/ColourHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace YesPojiQuota.Utils
+{
+    public static class ColourHelper
+    {
+        public static Color LightenDarkenColor(Color color, double correctionFactor)
+        {
+            double red = (255 - color.R) * correctionFactor + color.R;
+            double green = (255 - color.G) * correctionFactor + color.G;
+            double blue = (255 - color.B) * correctionFactor + color.B;
+            return Color.FromArgb(color.A, (byte)red, (byte)green, (byte)blue);
+        }
+    }
+}
