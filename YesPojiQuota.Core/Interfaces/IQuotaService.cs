d7144d84108780425d0598cbee46673a4d98afe9
xkre <xkre@live.com>
2017-05-12 18:16:49 +0800

    Change decimal to double

--- YesPojiQuota.Core/Interfaces/IQuotaService.cs
+++ YesPojiQuota.Core/Interfaces/IQuotaService.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YesPojiQuota.Core.Models;
 
 namespace YesPojiQuota.Core.Interfaces
 {
     public interface IQuotaService
     {
-        Task<decimal> GetQuota(string Username);
+        Task<double> GetQuota(string Username);
+        Task<double> GetQuota(Account a);
+
         double GetMaxQuota(string Username);
     }
 }
--- YesPojiQuota.Core/Models/Quota.cs
+++ YesPojiQuota.Core/Models/Quota.cs
@@ -4,7 +4,7 @@ namespace YesPojiQuota.Core.Models
     public class Quota
     {
         public int QuotaId { get; set; }
-        public decimal Available { get; set; }
+        public double Available { get; set; }
 
         public int AccountId { get; set; }
         public virtual Account Account { get; set; }
