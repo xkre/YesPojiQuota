cb7a78e039ca0dc6e54054cf3938e6e5b5f9fc61
xkre <arekram@live.com>
2017-05-24 18:25:54 +0800

    Added AppData model and added a few constants

---
+++ YesPojiQuota.Core/Models/AppData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YesPojiQuota.Core.Models
+{
+    public class AppData
+    {
+        public int DbVersion { get; set; }
+        public DateTime QuotaLastRefresed { get; set; }
+    }
+}
