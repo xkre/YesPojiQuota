fa1d5281c4d4becf9e18df8fc5874fce75f74ef1
xkre <arekram@live.com>
2017-06-21 14:49:13 +0800

    change interface signature

--- YesPojiQuota.Core/Interfaces/ILoginService.cs
+++ YesPojiQuota.Core/Interfaces/ILoginService.cs
@@ -8,6 +8,9 @@ namespace YesPojiQuota.Core.Interfaces
 {
     public interface ILoginService
     {
+        event LoginFailedEvent OnLoginFailed;
+        event SimpleEvent OnLoginSuccess;
+
         //Task<bool> LoginAsync(Account a);
         Task<bool> LoginAsync(string u, string p);
         Task<bool> LogoutAsync();
