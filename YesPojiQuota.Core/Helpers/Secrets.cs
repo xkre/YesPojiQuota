c4a352f229d9beafa56e222db0859f2389b3aafe
Akram Shaari <xkre@users.noreply.github.com>
2017-11-02 11:24:21 +0800

    Create Secrets.cs for appveyor

---
+++ YesPojiQuota.Core/Helpers/Secrets.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YesPojiQuota.Core.Helpers
+{
+    public static partial class Constants
+    {
+        public static string KEY = "Dummy";
+    }
+}
