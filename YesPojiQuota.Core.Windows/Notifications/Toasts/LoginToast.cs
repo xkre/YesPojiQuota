99accc1fd60b18970f4e994ee19f7f193e01a875
xkre <arekram@live.com>
2017-11-22 09:21:15 +0800

    Toast implementation

---
+++ YesPojiQuota.Core.Windows/Notifications/Toasts/LoginToast.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.QueryStringDotNET;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YesPojiQuota.Core.Models;
+
+namespace YesPojiQuota.Core.Windows.Notifications.Toasts
+{
+    internal class LoginToast : ToastBase
+    {
+        const string title = "Login to Yes4G Wifi";
+        const string content = "You are connected to Yes4G network, would you like to login?";
+
+        public LoginToast() : base (title, content)
+        {
+        }
+
+        protected override void CreateActions()
+        {
+            _actions = new ToastActionsCustom()
+            {
+                Inputs =
+                {
+                    new ToastSelectionBox("account")
+                },
+
+                Buttons =
+                {
+                    new ToastButton("Login", new QueryString()
+                    {
+                        { "action", "login" }
+                    }.ToString())
+                    {
+                        ActivationType = ToastActivationType.Background,
+                        //ImageUri = "Assets/Reply.png",
+
+                        // Reference the text box's ID in order to
+                        // place this button next to the text box
+                        //TextBoxId = "tbReply"
+                    }
+                }
+            };
+        }
+
+        public void SetAccounts(IList<Account> accounts)
+        {
+            if (accounts.Count > 0)
+            {
+                if (accounts.Count > 5)
+                    accounts = accounts.OrderBy(x=> x.Quota.Available).Take(5).ToList();
+
+                var toastSelectionBox = _actions.Inputs[0] as ToastSelectionBox;
+
+                foreach (var account in accounts)
+                {
+                    toastSelectionBox.Items.Add(new ToastSelectionBoxItem(account.Username, account.Username));
+                }
+
+                toastSelectionBox.DefaultSelectionBoxItemId = accounts[0].Username;
+            }
+            return;
+        }
+    }
+}
---
+++ YesPojiQuota.Core.Windows/Notifications/Toasts/ToastBase.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+
+namespace YesPojiQuota.Core.Windows.Notifications.Toasts
+{
+    public class ToastBase
+    {
+        public XmlDocument Xml => Content.GetXml();
+        public virtual ToastContent Content => _toastContent;
+
+        protected string _title;
+        protected string _content;
+
+        protected ToastContent _toastContent;
+
+        protected ToastActionsCustom _actions;
+        protected ToastVisual _visual;
+
+        public ToastBase(string title, string content)
+        {
+            _title = title;
+            _content = content;
+
+            CreateContent();
+            CreateActions();
+
+            _toastContent = new ToastContent()
+            {
+                Visual = _visual,
+                Actions = _actions
+            };
+        }
+
+        protected virtual void CreateContent()
+        {
+            _visual = new ToastVisual()
+            {
+                BindingGeneric = new ToastBindingGeneric()
+                {
+                    Children =
+                    {
+                        new AdaptiveText()
+                        {
+                            Text = _title,
+                        },
+                        new AdaptiveText()
+                        {
+                            Text = _content
+                        }
+                    }
+                }
+            };
+        }
+
+        protected virtual void CreateActions()
+        {
+            _actions = new ToastActionsCustom();
+        }
+    }
+}
