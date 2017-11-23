bc6e124902e344636642792ed349c1427b3e338a
xkre <arekram@live.com>
2017-11-23 15:46:56 +0800

    Added some classes for Live tiles

---
+++ YesPojiQuota.Core.Windows/Notifications/Live Tiles/LiveTileBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YesPojiQuota.Core.Windows.Notifications.Live_Tiles
+{
+    public class LiveTileBase
+    {
+    }
+}
---
+++ YesPojiQuota.Core.Windows/Notifications/Toasts/QuotaToast.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.QueryStringDotNET;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YesPojiQuota.Core.Windows.Notifications.Toasts
+{
+    public class QuotaToast : ToastBase
+    {
+        private const string title = "Logged In";
+        private const string content = "Quota Remaining";
+
+        public QuotaToast() : base(title, content)
+        {
+        }
+
+        protected override void CreateContent()
+        {
+            base.CreateContent();
+
+            _actions = new ToastActionsCustom()
+            {
+                Buttons =
+                {
+                    new ToastButton("Logout", new QueryString()
+                    {
+                        { "action", "logout" }
+                    }.ToString())
+                    {
+                        ActivationType = ToastActivationType.Background,
+                        //ImageUri = "Assets/Reply.png",
+
+                        // Reference the text box's ID in order to
+                        // place this button next to the text box
+                        //TextBoxId = "tbReply"
+                    }
+                }
+            };
+        }
+    }
+}
